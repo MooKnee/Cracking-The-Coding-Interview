c6282bdd56bb3a0935bc964385e25fc49ecbb278
Catie Mooney <42590833+MooKnee@users.noreply.github.com>
2021-07-20 01:11:52 -0400

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,9 @@
+#1.1
+#Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+#cannot use additional data structures?
+
+def main():
+    print("Make GitHub run")
+    
+main()
+#doot
